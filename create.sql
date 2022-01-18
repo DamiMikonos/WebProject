6fa1b1d5c2f2c1bb4246886321d16c1bbb5969d9
marioukoss <mariospap900@gmail.com>
2022-01-19 01:44:01 +0200

    POI changes
    Προσθεση πινακα Activity σχετικα με τα POI

--- create.sql
+++ create.sql
@@ -22,12 +22,41 @@ CREATE TABLE POI(
     rating FLOAT,
     rating_n INT,
     current_popularity INT,
-    day VARCHAR (15),
-    data INT,
     time_spent INT,
     time_wait INT,
     PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS ACTIVITY;
+CREATE TABLE ACTIVITY(
+		poiid VARCHAR (50) NOT NULL,
+		day VARCHAR (10) NOT NULL,
+		hour1 INT NOT NULL,
+		hour2 INT NOT NULL,
+		hour3 INT NOT NULL,
+		hour4 INT NOT NULL,
+		hour5 INT NOT NULL,
+		hour6 INT NOT NULL,
+		hour7 INT NOT NULL,
+		hour8 INT NOT NULL,
+		hour9 INT NOT NULL,
+		hour10 INT NOT NULL,
+		hour11 INT NOT NULL,
+		hour12 INT NOT NULL,
+		hour13 INT NOT NULL,
+		hour14 INT NOT NULL,
+		hour15 INT NOT NULL,
+		hour16 INT NOT NULL,
+		hour17 INT NOT NULL,
+		hour18 INT NOT NULL,
+		hour19 INT NOT NULL,
+		hour20 INT NOT NULL,
+		hour21 INT NOT NULL,
+		hour22 INT NOT NULL,
+		hour23 INT NOT NULL,
+		hour24 INT NOT NULL,
+		FOREIGN KEY (poiid) REFERENCES POI(id)
+	);
 
 DROP TABLE IF EXISTS Cases;
 CREATE  TABLE Cases(
